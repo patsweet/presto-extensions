52d75fabd58f5b0901435b31a93a4f882d30f1da
Patrick Sweet <patmsweet@gmail.com>
2013-07-16 21:56:23 -0400

    Added preventDefault
    Got rid of the id selector because it could differ from training to production sites. The a[title='Remove'] selector worked on the training site.

--- presto-extensions/presto-extensions/clearfront.js
+++ presto-extensions/presto-extensions/clearfront.js
@@ -1,8 +1,9 @@
 $("div.controlButtons :first")
     .before(function() {
         return $("<a id='clearfrontcontent' class='RadButton RadButton_PrestoRadSkin rbSkinnedButton button' href='#'>Clear Front</a>")
-            .click(function() {
-                $("#dnn_ctr635_View_LMC_7870 a[title='Remove']").each(function(){
+            .click(function(e) {
+                e.preventDefault();
+                $("a[title='Remove']").each(function(){
                     this.click();
                 });
             });
