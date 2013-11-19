3140b7bc73e4500399a0aebd8e4fd8aac3a23136
Patrick Sweet <psweet@delawareonline.com>
2013-11-19 18:00:44 -0500

    Fixed selector for close buttons

--- presto-extensions/presto-extensions/clearfront.js
+++ presto-extensions/presto-extensions/clearfront.js
@@ -1,9 +1,11 @@
 $("div.controlButtons :first")
     .before(function() {
-        return $("<a id='clearfrontcontent' class='RadButton RadButton_PrestoRadSkin rbSkinnedButton button' href='#'>Clear Front</a>")
-            .click(function() {
-                $("#dnn_ctr635_View_LMC_7870 a[title='Remove']").each(function(){
+        return $("<a id='clearfrontcontent' class='RadButton RadButton_PrestoRadSkin rbSkinnedButton button' href='#' title='Click this to clear the current front. Nothing is permanent, though, until you click Publish.'>Clear Front</a>")
+            .click(function(e) {
+                e.preventDefault();
+                $("ul.rdCommands span.rdClose").each(function(){
                     this.click();
                 });
+                return false;
             });
     });
