0605bbdb49a8f2f6b3076371c84823786ecf8f8a
Patrick Sweet <psweet@delawareonline.com>
2014-01-09 12:27:08 -0500

    Add ability to download multiple photo types.

--- presto-extensions/presto-extensions/downloadPhotoAsset.js
+++ presto-extensions/presto-extensions/downloadPhotoAsset.js
@@ -1,21 +1,27 @@
-var asset_url_regex = /\/media.+\.jpg/i;
-function parseUrl(asset_url) {
-    var matches = asset_url.match(asset_url_regex);
-    if (matches) {
-        return matches[0];
-    } else {
-        throw new Error("Can't parse url of image asset.");
+function getObjects(obj, key, val) {
+    // http://stackoverflow.com/a/4992429/1309020
+    // Used to snag items from json by a given value.
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else if (i == key && obj[key] == val) {
+            objects.push(obj);
+        }
     }
+    return objects;
 }
-
 $("#assetsPane ul.rlbList li span.paneIcon.image").each(function() {
     var $li = $(this).closest("li");
-    var asset_url = $li.find(".assetsImage img").attr('src');
-    if (asset_url) {
-        var $download_btn = $("<div class='assetsInformation'><a href='http://www.gannett-cdn.com"+ parseUrl(asset_url) +"' target='_blank'>Download</a></div>")
-            .css('text-align', 'center');
+    var json_url = window.location.origin + $li.find(".assetsInformation a:contains(json)").attr('href');
+    $.getJSON(json_url, function(data) {
+        var baseurl = getObjects(data['attributes'], 'name', 'publishurl')[0]['value'];
+        var xsl = baseurl + getObjects(data['ratios'], 'name', 'x-superlarge')[0]['path'];
+        var xxl = baseurl + getObjects(data['ratios'], 'name', 'x-extralarge')[0]['path'];
+        var xl = baseurl + getObjects(data['ratios'], 'name', 'x-large')[0]['path'];
+        var norm = baseurl + getObjects(data['ratios'], 'name', 'x')[0]['path'];
+        var $download_btn = $("<div class='assetsInformation'>Download: <a href='"+ xsl +"' target='_blank' title='x-superlarge'>xsl</a> | <a href='"+ xxl +"' target='_blank' title='x-extralarge'>xxl</a> | <a href='"+ xl +"' target='_blank' title='x-large'>xl</a> | <a href='"+ norm +"' target='_blank' title='normal'>l</a></div>");
         $li.find(".assetsBlurb").after($download_btn);
-    } else {
-        throw new Error("Mismatch occured. Image url not available.");
-    }
+    });
 });
